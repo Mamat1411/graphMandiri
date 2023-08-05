8143585f075617016572a4e289ffad5856b3994e
Mamat1411 <muhammadmujahid14@gmail.com>
2023-08-05 15:17:04 +0700

    Exporting Latest SQL

--- graphmandiri.sql
+++ graphmandiri.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 11, 2020 at 03:07 AM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.3
+-- Generation Time: Aug 05, 2023 at 10:15 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -34,8 +33,8 @@ CREATE TABLE `failed_jobs` (
   `queue` text NOT NULL,
   `payload` longtext NOT NULL,
   `exception` longtext NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -47,7 +46,7 @@ CREATE TABLE `migrations` (
   `id` int(10) UNSIGNED NOT NULL,
   `migration` varchar(255) NOT NULL,
   `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `migrations`
@@ -68,7 +67,7 @@ CREATE TABLE `password_resets` (
   `email` varchar(255) NOT NULL,
   `token` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -82,7 +81,7 @@ CREATE TABLE `tb_casa` (
   `tanggal` date NOT NULL,
   `realisasi` int(15) NOT NULL,
   `target` int(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -96,7 +95,7 @@ CREATE TABLE `tb_deposito` (
   `tanggal` date NOT NULL,
   `realisasi` int(15) NOT NULL,
   `target` int(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tb_deposito`
@@ -120,7 +119,7 @@ CREATE TABLE `tb_dpk` (
   `tanggal` date NOT NULL,
   `realisasi` int(15) NOT NULL,
   `target` int(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -134,7 +133,7 @@ CREATE TABLE `tb_giro` (
   `tanggal` date NOT NULL,
   `realisasi` int(15) NOT NULL,
   `target` int(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tb_giro`
@@ -158,7 +157,7 @@ CREATE TABLE `tb_kantor` (
   `nama_cabang` varchar(100) NOT NULL,
   `kelas_cabang` varchar(100) NOT NULL,
   `lokasi` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tb_kantor`
@@ -169,7 +168,8 @@ INSERT INTO `tb_kantor` (`kode_cabang`, `nama_cabang`, `kelas_cabang`, `lokasi`)
 (14402, 'KC Malang JA Suprapto', 'Kelas 1', 'Jl. JA Suprapto'),
 (14403, 'KC Batu', 'Kelas 1', 'Batu'),
 (14404, 'KC Pasuruan', 'Kelas 1', 'Pasuruan'),
-(14408, 'KCP Malang Soekarno Hatta', 'Kelas 2', 'Jl. Soekarno Hatta Malang');
+(14408, 'KCP Malang Soekarno Hatta', 'Kelas 2', 'Jl. Soekarno Hatta Malang'),
+(14410, 'KC Depok Sleman', 'Kelas 1', 'Depok Sleman Yogyakarta');
 
 -- --------------------------------------------------------
 
@@ -183,7 +183,7 @@ CREATE TABLE `tb_tabungan` (
   `tanggal` date NOT NULL,
   `realisasi` int(11) NOT NULL,
   `target` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tb_tabungan`
@@ -209,9 +209,9 @@ CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `users`
@@ -306,7 +306,7 @@ CREATE TABLE `viewtabungan` (
 --
 DROP TABLE IF EXISTS `viewcasa`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `viewcasa`  AS  select `tb_tabungan`.`kode_cabang` AS `kode_cabang`,`tb_kantor`.`nama_cabang` AS `nama_cabang`,`tb_tabungan`.`tanggal` AS `tanggal`,(`tb_tabungan`.`realisasi` + `tb_giro`.`realisasi`) AS `realisasi`,(`tb_tabungan`.`target` + `tb_giro`.`target`) AS `target`,concat(round((((`tb_tabungan`.`realisasi` + `tb_giro`.`realisasi`) / (`tb_tabungan`.`target` + `tb_giro`.`target`)) * 100),2)) AS `prosentase` from ((`tb_tabungan` join `tb_kantor` on((`tb_tabungan`.`kode_cabang` = `tb_kantor`.`kode_cabang`))) join `tb_giro` on((`tb_kantor`.`kode_cabang` = `tb_giro`.`kode_cabang`))) where ((`tb_tabungan`.`tanggal` = `tb_giro`.`tanggal`) and (`tb_tabungan`.`kode_cabang` = `tb_giro`.`kode_cabang`)) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `viewcasa`  AS SELECT `tb_tabungan`.`kode_cabang` AS `kode_cabang`, `tb_kantor`.`nama_cabang` AS `nama_cabang`, `tb_tabungan`.`tanggal` AS `tanggal`, `tb_tabungan`.`realisasi`+ `tb_giro`.`realisasi` AS `realisasi`, `tb_tabungan`.`target`+ `tb_giro`.`target` AS `target`, concat(round((`tb_tabungan`.`realisasi` + `tb_giro`.`realisasi`) / (`tb_tabungan`.`target` + `tb_giro`.`target`) * 100,2)) AS `prosentase` FROM ((`tb_tabungan` join `tb_kantor` on(`tb_tabungan`.`kode_cabang` = `tb_kantor`.`kode_cabang`)) join `tb_giro` on(`tb_kantor`.`kode_cabang` = `tb_giro`.`kode_cabang`)) WHERE `tb_tabungan`.`tanggal` = `tb_giro`.`tanggal` AND `tb_tabungan`.`kode_cabang` = `tb_giro`.`kode_cabang``kode_cabang`  ;
 
 -- --------------------------------------------------------
 
@@ -315,7 +315,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `viewdeposito`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `viewdeposito`  AS  select `tb_deposito`.`id_deposito` AS `id_deposito`,`tb_deposito`.`kode_cabang` AS `kode_cabang`,`tb_kantor`.`nama_cabang` AS `nama_cabang`,`tb_deposito`.`tanggal` AS `tanggal`,`tb_deposito`.`realisasi` AS `realisasi`,`tb_deposito`.`target` AS `target`,concat(round(((`tb_deposito`.`realisasi` / `tb_deposito`.`target`) * 100),2)) AS `prosentase` from (`tb_kantor` join `tb_deposito` on((`tb_kantor`.`kode_cabang` = `tb_deposito`.`kode_cabang`))) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `viewdeposito`  AS SELECT `tb_deposito`.`id_deposito` AS `id_deposito`, `tb_deposito`.`kode_cabang` AS `kode_cabang`, `tb_kantor`.`nama_cabang` AS `nama_cabang`, `tb_deposito`.`tanggal` AS `tanggal`, `tb_deposito`.`realisasi` AS `realisasi`, `tb_deposito`.`target` AS `target`, concat(round(`tb_deposito`.`realisasi` / `tb_deposito`.`target` * 100,2)) AS `prosentase` FROM (`tb_kantor` join `tb_deposito` on(`tb_kantor`.`kode_cabang` = `tb_deposito`.`kode_cabang`))  ;
 
 -- --------------------------------------------------------
 
@@ -324,7 +324,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `viewdpk`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `viewdpk`  AS  select `tb_tabungan`.`kode_cabang` AS `kode_cabang`,`tb_kantor`.`nama_cabang` AS `nama_cabang`,`tb_tabungan`.`tanggal` AS `tanggal`,((`tb_tabungan`.`realisasi` + `tb_giro`.`realisasi`) + `tb_deposito`.`realisasi`) AS `realisasi`,((`tb_tabungan`.`target` + `tb_giro`.`target`) + `tb_deposito`.`target`) AS `target`,concat(round(((((`tb_tabungan`.`realisasi` + `tb_giro`.`realisasi`) + `tb_deposito`.`realisasi`) / ((`tb_tabungan`.`target` + `tb_giro`.`target`) + `tb_deposito`.`target`)) * 100),2)) AS `prosentase` from (((`tb_tabungan` join `tb_kantor` on((`tb_tabungan`.`kode_cabang` = `tb_kantor`.`kode_cabang`))) join `tb_giro` on((`tb_kantor`.`kode_cabang` = `tb_giro`.`kode_cabang`))) join `tb_deposito` on((`tb_kantor`.`kode_cabang` = `tb_deposito`.`kode_cabang`))) where ((`tb_tabungan`.`tanggal` = `tb_giro`.`tanggal`) and (`tb_giro`.`tanggal` = `tb_deposito`.`tanggal`) and (`tb_tabungan`.`kode_cabang` = `tb_giro`.`kode_cabang`) and (`tb_giro`.`kode_cabang` = `tb_deposito`.`kode_cabang`)) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `viewdpk`  AS SELECT `tb_tabungan`.`kode_cabang` AS `kode_cabang`, `tb_kantor`.`nama_cabang` AS `nama_cabang`, `tb_tabungan`.`tanggal` AS `tanggal`, `tb_tabungan`.`realisasi`+ `tb_giro`.`realisasi` + `tb_deposito`.`realisasi` AS `realisasi`, `tb_tabungan`.`target`+ `tb_giro`.`target` + `tb_deposito`.`target` AS `target`, concat(round((`tb_tabungan`.`realisasi` + `tb_giro`.`realisasi` + `tb_deposito`.`realisasi`) / (`tb_tabungan`.`target` + `tb_giro`.`target` + `tb_deposito`.`target`) * 100,2)) AS `prosentase` FROM (((`tb_tabungan` join `tb_kantor` on(`tb_tabungan`.`kode_cabang` = `tb_kantor`.`kode_cabang`)) join `tb_giro` on(`tb_kantor`.`kode_cabang` = `tb_giro`.`kode_cabang`)) join `tb_deposito` on(`tb_kantor`.`kode_cabang` = `tb_deposito`.`kode_cabang`)) WHERE `tb_tabungan`.`tanggal` = `tb_giro`.`tanggal` AND `tb_giro`.`tanggal` = `tb_deposito`.`tanggal` AND `tb_tabungan`.`kode_cabang` = `tb_giro`.`kode_cabang` AND `tb_giro`.`kode_cabang` = `tb_deposito`.`kode_cabang``kode_cabang`  ;
 
 -- --------------------------------------------------------
 
@@ -333,7 +333,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `viewgiro`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `viewgiro`  AS  select `tb_giro`.`id_giro` AS `id_giro`,`tb_giro`.`kode_cabang` AS `kode_cabang`,`tb_kantor`.`nama_cabang` AS `nama_cabang`,`tb_giro`.`tanggal` AS `tanggal`,`tb_giro`.`realisasi` AS `realisasi`,`tb_giro`.`target` AS `target`,concat(round(((`tb_giro`.`realisasi` / `tb_giro`.`target`) * 100),2)) AS `prosentase` from (`tb_kantor` join `tb_giro` on((`tb_kantor`.`kode_cabang` = `tb_giro`.`kode_cabang`))) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `viewgiro`  AS SELECT `tb_giro`.`id_giro` AS `id_giro`, `tb_giro`.`kode_cabang` AS `kode_cabang`, `tb_kantor`.`nama_cabang` AS `nama_cabang`, `tb_giro`.`tanggal` AS `tanggal`, `tb_giro`.`realisasi` AS `realisasi`, `tb_giro`.`target` AS `target`, concat(round(`tb_giro`.`realisasi` / `tb_giro`.`target` * 100,2)) AS `prosentase` FROM (`tb_kantor` join `tb_giro` on(`tb_kantor`.`kode_cabang` = `tb_giro`.`kode_cabang`))  ;
 
 -- --------------------------------------------------------
 
@@ -342,7 +342,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `viewtabungan`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `viewtabungan`  AS  select `tb_tabungan`.`id_tabungan` AS `id_tabungan`,`tb_tabungan`.`kode_cabang` AS `kode_cabang`,`tb_kantor`.`nama_cabang` AS `nama_cabang`,`tb_tabungan`.`tanggal` AS `tanggal`,`tb_tabungan`.`realisasi` AS `realisasi`,`tb_tabungan`.`target` AS `target`,concat(round(((`tb_tabungan`.`realisasi` / `tb_tabungan`.`target`) * 100),2)) AS `prosentase` from (`tb_kantor` join `tb_tabungan` on((`tb_kantor`.`kode_cabang` = `tb_tabungan`.`kode_cabang`))) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `viewtabungan`  AS SELECT `tb_tabungan`.`id_tabungan` AS `id_tabungan`, `tb_tabungan`.`kode_cabang` AS `kode_cabang`, `tb_kantor`.`nama_cabang` AS `nama_cabang`, `tb_tabungan`.`tanggal` AS `tanggal`, `tb_tabungan`.`realisasi` AS `realisasi`, `tb_tabungan`.`target` AS `target`, concat(round(`tb_tabungan`.`realisasi` / `tb_tabungan`.`target` * 100,2)) AS `prosentase` FROM (`tb_kantor` join `tb_tabungan` on(`tb_kantor`.`kode_cabang` = `tb_tabungan`.`kode_cabang`))  ;
 
 --
 -- Indexes for dumped tables
